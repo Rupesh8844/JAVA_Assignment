fb2f6c9650f8380799aa61a8c75153d59cd193e9
unknown <rupeshthakur8844@gmail.com>
2022-05-18 21:44:18 +0530

    renamed

--- Day2/src/com/masai/Assignment4.java
+++ Day2/src/com/masai/Assignment4.java
@@ -1,7 +1,7 @@
 package com.masai;
 
 public class Assignment4 {
-	  public static void myMethod(int num1 ,int num2 , int num3 , int num4 , int num5 ) {
+	  public static void myMethod(int num1 ,int num2 ,int num3 , int num4 , int num5 ) {
 		 int total = (num1*1)+(num2*2)+(num3*3)+(num4*4)+(num5*6);
 		  System.out.println(total);
 	  }
